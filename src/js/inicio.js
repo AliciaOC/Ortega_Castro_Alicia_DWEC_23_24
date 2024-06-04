3aa8ab533bf1ac88b4126844eee781c5dc808020
AliciaOC <96443722+AliciaOC@users.noreply.github.com>
2024-06-04 21:24:04 +0200

    funciona el filtro por categorias, el de orden por precio y las vistas simultáneamente sin errores

--- src/js/inicio.js
+++ src/js/inicio.js
@@ -1,6 +1,34 @@
+borrarFiltroCategoria();
 cargarCategoriasNav();
 cargarProductos();
-//Para cambiar a vista de lista
+
+document.getElementById('enlace-productos').addEventListener('click', ()=>{
+    if(comprobarFiltroCategoria()){
+        borrarFiltroCategoria();
+    }
+    cargarProductos();
+});
+
+//Para cambiar el orden
+//va a ser un problema porque con el scroll infinito se repiten los productos ya que solo hay 20
+document.getElementById('ordenar').addEventListener('click',()=>{
+    //Primer click para abrir las opciones, no lo tengo en cuenta pero necesito escucharlo para el segundo click
+    //Segundo click para cambiar el orden
+    document.getElementById('ordenar').addEventListener('click',()=>{
+        //Reviso si ha cambiado el orden
+        if(orden!=document.getElementById('ordenar').value){
+            orden = document.getElementById('ordenar').value;
+            //miro si ha filtrado por categoria o no
+            if(comprobarFiltroCategoria()){
+                cargarProductos(comprobarFiltroCategoria());
+            }else{
+                cargarProductos();
+            }
+        }
+    });
+});
+
+//Para cambiar la vista
 document.getElementById('vista').addEventListener('click',()=>{
 //primer click para abrir las opciones
 //al segundo click puede que haya cambiado la vista
@@ -8,9 +36,15 @@ document.getElementById('vista').addEventListener('click',()=>{
         //reviso si ha cambiado la vista
         if(vista!=document.getElementById('vista').value){
         vista = document.getElementById('vista').value;
-        cargarProductos();
+        //miro si ha filtrado por categoria o no
+        if(comprobarFiltroCategoria()){
+            cargarProductos(comprobarFiltroCategoria());
+        }else{
+            cargarProductos();
+        }
         }
     });
 });
-window.addEventListener('scroll', scrollInfinito);
+
+//window.addEventListener('scroll', scrollInfinito);
 
--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ const URL_PRODUCTOS="https://fakestoreapi.com/products";
 const BOTON_LOGOUT = document.getElementById("boton-logout");
 
 let categoriasNav = document.getElementById("categorias-nav");
+let botonesCategorias = document.getElementsByClassName('categoria-boton');
 let productosSection = document.getElementById("productos");
 let vista= document.getElementById("vista").value;
 let orden= document.getElementById("ordenar").value;
@@ -20,19 +21,25 @@ fetch(URL_CATEGORIAS)
     categorias.forEach(categoria => {
         // Crear un nuevo elemento <button>
         let boton = document.createElement('button');
+        boton.classList.add('categoria-boton');
         // Asignar el nombre de la categoría
         boton.innerHTML = categoria;
-        // Agregar el evento click al botón
+        // Agregar un evento al botón con click, se ejecuta la funcion cargarProductos con el nombre de la categoría
         boton.addEventListener('click', () => {
-            cargarProductosCategoria(categoria);
+            if(comprobarFiltroCategoria()){
+                borrarFiltroCategoria();
+            }
+            boton.classList.add('categoria-seleccionada');
+            cargarProductos(boton.innerHTML);
         });
-        // Agregar el botón al div de categorías
+        // Agregar el botón al nav de categorías
         categoriasNav.appendChild(boton);
     });
 })
 }
 
-function cargarProductos(categoria=0){//Por defecto no se le pasa ninguna categoría
+function cargarProductos(categoria){
+    if(categoria==null){//Si no se le pasa ninguna categoría, carga todos los productos
     fetch(URL_PRODUCTOS) 
         .then(res=>res.json())
         .then(productos=>{
@@ -42,9 +49,27 @@ function cargarProductos(categoria=0){//Por defecto no se le pasa ninguna catego
         });
         mostrarproductos(productos);
     });
+    }else{//Si se le pasa una categoría, carga los productos de esa categoría 
+        fetch(`${URL_PRODUCTOS}/category/${categoria}`)
+        .then(res=>res.json())
+        .then(productos=>{
+            productos.forEach(producto => {
+                producto.gusta = 0;
+                producto.nogusta = 0;
+            });
+            mostrarproductos(productos);
+        });
+    }  
 }
 
 function mostrarproductos(productos){
+    //Ordenar los productos por precio
+    if(orden=="ascendente"){
+        productos.sort((a,b)=>a.price-b.price);
+    }else if(orden=="descendente"){
+        productos.sort((a,b)=>b.price-a.price);
+    }
+    //Mostrar los productos en la vista seleccionada
     if(vista == "tabla"){
         distribucionTabla(productos);
     }else if(vista == "lista"){
@@ -52,21 +77,23 @@ function mostrarproductos(productos){
     }
 }
 
-function cargarProductosCategoria(categoria){}
-
-
 function distribucionTabla(productos){
     //borro por si había ya algo
     productosSection.innerHTML="";
 
     let tabla = document.createElement('table');
-    let numFilas= Math.ceil(productos.length/4);//hay 20 productos en total, lo divido así para que haga filas completas
+    let numFilas= Math.ceil(productos.length/4);//hay 20 productos en total, lo divido así para que haga filas completas. Y en la categoria de menos productos hay 4
 
     let contador=0;//Este contador es para llevar la posición del array de productos durante todo el bucle
 
     for(let i=0;i<numFilas;i++){
         let fila = document.createElement('tr');
         for(let j=0;j<4;j++){
+            //Si no hay más productos, salgo del bucle (en las categorias de electronica y ropa de mujer hace falta)
+            if(contador>=productos.length){
+                break;
+            }
+
             let celda = document.createElement('td');
             let producto = productos[contador];
             celda.classList.add('casilla-producto');//Para estilos
@@ -110,6 +137,25 @@ function distribucionLista(productos){
     productosSection.appendChild(lista);
 }
 
+function comprobarFiltroCategoria(){
+    for (let boton of botonesCategorias) {
+        if(boton.classList.contains('categoria-seleccionada')){//contains devuelve true si la clase está en el elemento
+            let categoria= boton.innerHTML;
+            return categoria;
+        }
+    }
+    return false;
+}
+
+function borrarFiltroCategoria(){
+    for (let boton of botonesCategorias) {
+        if(boton.classList.contains('categoria-seleccionada')){
+            boton.classList.remove('categoria-seleccionada');
+        }
+    }
+}
+
+/*
 function scrollInfinito() {
     // Obtener la posición actual del scroll
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;//scrollTop: posición actual del scroll, scrollHeight: altura total de la página, clientHeight: altura de la ventana del navegador. Las llaves indican que se están extrayendo las propiedades de document.documentElement
@@ -118,5 +164,5 @@ function scrollInfinito() {
         cargarProductos();//Cargar más posts
     }
 }
-
+*/
 //fin funciones
